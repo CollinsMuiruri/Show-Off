3f87da790e4aea151fb80c18eb5213ad481fd7f3
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-22 14:54:17 +0300

    Configure location foreign key

--- show/off/models.py
+++ show/off/models.py
@@ -18,6 +18,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'articles/')
     image_name = models.CharField(max_length =60)
     image_description = models.TextField()
+    location = models.ForeignKey(Location)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
