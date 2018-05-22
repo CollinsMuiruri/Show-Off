add18d7fff469329a591670ed60f361a41295bbf
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-22 14:48:13 +0300

    Create Location and Category models

--- show/off/models.py
+++ show/off/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/')
+    image_name = models.CharField(max_length =60)
+    image_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image
